cd17fb4c23bb2e1cf3614abbabbb28bfcb088f54
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2023-11-15 15:50:45 +0100

    Agregar archivos de proyecto.

---
+++ ProgramGame_Jocdelpenjat/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Author: Jordi Sancho Garcia
+ * M03. Programació UF1
+ * v1. 18/10/2023
+ * PR1. Joc de Batalla d'Herois vs Monstre
+ *
+ */
+
+namespace GameProject
+{
+    public class SanchoJordiCode
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
